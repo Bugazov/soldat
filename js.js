86c3ca5fcb96cf70e9d6e89b12cdc5ad97b7c797
OdinTakoy <bugazov9@mail.ru>
2022-07-12 14:48:19 +0300

    Доработал методы объектов

--- js.js
+++ js.js
@@ -1,12 +1,32 @@
 const solider ={
     name:'Islam',
-    zdrav: 10,
+    zdrav: 2,
     weapon: {
         nameweapon: "Avtomat",
         number:30
     },
     supplies:3,
-    shoot:function(){},
-    recharge:function(){},
-    toHurt:function(){},
+    shoot:function(){
+        if(this.weapon.number ==0){
+            console.log('Обойма пуста,перезарядитесь')
+        }else{
+        this.weapon.number --;
+        console.log('Бах - бах')}
+    },
+    recharge:function(){
+        if(this.supplies == 0){
+            console.log("Не осталось припасов")
+        }else{
+            this.weapon.number = 0;
+            this.supplies--
+            console.log('перезарядка...')
+        }
+    },
+    toHurt:function(){
+        this.zdrav--
+        if(this.zdrav ==0){
+            console.log("Ты проиграл")
+        }
+    },
 }
+solider.toHurt()
