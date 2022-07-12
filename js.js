7b63da2e443b752639242de00810d1dd6c948cb7
OdinTakoy <bugazov9@mail.ru>
2022-07-12 14:35:42 +0300

    Дал название свойствам объектам

---
+++ js.js
@@ -0,0 +1,12 @@
+const solider ={
+    name:'Islam',
+    zdrav: 10,
+    weapon: {
+        nameweapon: "Avtomat",
+        number:30
+    },
+    supplies:3,
+    shoot:function(){},
+    recharge:function(){},
+    toHurt:function(){},
+}
